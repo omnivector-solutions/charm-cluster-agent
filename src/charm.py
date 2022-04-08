55f74115abfdf05dc90e7dda1dda95a4a720572d
Tucker Beck <tucker.beck@gmail.com>
2022-04-08 01:54:02 +0000

    Added slurm-user-mapper setting

--- src/charm.py
+++ src/charm.py
@@ -97,6 +97,7 @@ class ClusterAgentCharm(CharmBase):
             "auth0-audience": True,
             "auth0-client-id": True,
             "auth0-client-secret": True,
+            "slurm-user-mapper": False,
             "ldap-domain": False,
             "ldap-username": False,
             "ldap-password": False,
